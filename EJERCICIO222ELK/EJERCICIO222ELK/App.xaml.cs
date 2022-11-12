57878ac06ee7b7e8038ea788ff70802c6ed04614
ElkYTHN <elkinportillo4@gmail.com>
2022-11-12 11:00:02 -0600

    Agregar archivos de proyecto.

---
+++ EJERCICIO222ELK/EJERCICIO222ELK/App.xaml.cs
@@ -0,0 +1,44 @@
+﻿using EJERCICIO222ELK.Controllers;
+using System;
+using System.IO;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace EJERCICIO222ELK
+{
+    public partial class App : Application
+    {
+        static FirmaDB BaseDatos;
+
+        public static FirmaDB BaseDatosObject
+        {
+            get
+            {
+                if (BaseDatos == null)
+                {
+                    BaseDatos = new FirmaDB(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "FirmasDB.db3"));
+                }
+                return BaseDatos;
+            }
+        }
+
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new NavigationPage(new MainPage());
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ EJERCICIO222ELK/EJERCICIO222ELK/Models/FirmaModel.cs
@@ -0,0 +1,19 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EJERCICIO222ELK.Models
+{
+    public class FirmaModel
+    {
+
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        public string Nombre { get; set; }
+
+        public string Descripcion { get; set; }
+        public Byte[] Firma { get; set; }
+    }
+}
